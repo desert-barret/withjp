95e79103c0038b12b7f09be2a94ad4616887b7b0
Juan Pablo (Jp) <juanpa.desert@gmail.com>
2026-04-09 22:41:23 -0500

    v

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { createUnhead } from '@unhead/vue';
+import { createHead } from '@unhead/vue';
 import App from './App.vue';
 import router from './router';
 import { i18n } from './i18n';
 import './assets/main.css';
 
 const app = createApp(App);
-createUnhead();
+const head = createHead();
+app.use(head);
 
 app.config.errorHandler = (err, _instance, info) => {
   console.error('[Vue]', info, err);
