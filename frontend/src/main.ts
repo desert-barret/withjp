ee6c81e5c94583557b681fe87e7fce3fbe67995b
Juan Pablo (Jp) <juanpa.desert@gmail.com>
2026-04-09 22:39:09 -0500

    v

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { createUnhead } from 'unhead';
+import { createUnhead } from '@unhead/vue';
 import App from './App.vue';
 import router from './router';
 import { i18n } from './i18n';
